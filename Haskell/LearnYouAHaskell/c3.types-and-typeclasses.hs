473e1e7ffd0c8902f6493754ebf62c1d6aa765b0
ForkBombGIT <erosdipede@gmail.com>
2020-02-21 12:02:33 -0500

    Add C3 code for LearnYouAHaskell

--- Haskell/LearnYouAHaskell/c3.types-and-typeclasses.hs
+++ Haskell/LearnYouAHaskell/c3.types-and-typeclasses.hs
@@ -22,3 +22,26 @@ circumferenceFloat r = 2 * pi * r
 circumferenceDouble :: Double -> Double  
 circumferenceDouble r = 2 * pi * r  
 
+-- |Type variables
+-- The takeHead function takes a type variable (similar to a generic) array, and returns the first element of the array
+takeHead :: [a] -> a
+takeHead (h:t) = h
+
+-- |Typeclasses
+-- findValue is a recursive function that asserts the class constraint of Eq and takes a type variable and a type variable array
+-- which returns true if the variable is found within the array
+-- A class constraint is used to ensure that the type variables passed in are members of the class constraint applied, in this case the Eq class.
+-- The Eq typeclass provides an interface for testing for equality. 
+-- Any type where it makes sense to test for equality between two values of that type should be a member of the Eq class. 
+-- All standard Haskell types except for IO (the type for dealing with input and output) and functions are a part of the Eq typeclass.
+findValue :: (Eq a) => a -> [a] -> Bool
+findValue _ [] = False
+findValue x [h] = x == h
+findValue x (h:t) 
+    | (x == h) = True
+    | otherwise = findValue x t
+
+-- addVals adds two Num class values and returns the result
+-- Num is a numeric typeclass. Its members have the property of being able to act like numbers. Let's examine the type of a number.
+addVals :: (Num a) => a -> a -> a
+addVals x y = x + y
---
+++ Haskell/LearnYouAHaskell/c4.syntax-in-functions.hs
@@ -0,0 +1,4 @@
+-- |Learn You a Haskell
+--  Chapter 4: Syntax in Functions
+
+-- |Pattern Matching
